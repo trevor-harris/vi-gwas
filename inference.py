fc3deb884e0a2f3cf5c27237f64c09768fbbcbed
Trevor Harris <trevorh2@illinois.edu>
2017-03-22 00:11:03 -0500

    switched from horseshoe priors to the much more convenient laplace prior (equivalent to L1 regularization).

--- inference.py
+++ inference.py
@@ -6,104 +6,29 @@ from pystan import StanModel
 from math import exp
 
 # data shape
-p = 1000
-true_p = 10
-n = 100
-
-# generate SNPs
-maf = np.random.uniform(0.05, 0.5, p)
-snps = np.empty(shape = (n, p))
-
-for ind, f in np.ndenumerate(maf):
-	snps[:, ind[0]] = np.random.binomial(2, f, n)
-
-# normalize snps
-snps = (snps - snps.mean(axis = 0)) / snps.std(axis = 0)
-
-# generate genotypes
-# select a subset to be in the true model
-true_id = np.arange(1, p, int(p/true_p))
-true_snps = snps[:, true_id]
-true_beta = np.random.choice(np.array([-3, -2, -1, 1, 2, 3]), true_p)
-true_alpha = -0.2
-
-print(true_beta)
-
-# create gene expression levels
-def binomialize(x):
-	p = 1 / (1 + exp(-x))
-	return np.random.binomial(1, p, 1)
-vbin = np.vectorize(binomialize)
-
-geno = np.sum(true_alpha + true_snps * true_beta, axis = 1)
-geno = vbin(geno)
+p = 300
+true_p = 5
+n = 1000
 
 
-#  # Begin Stan section
-# gwas_code = '''
-# data {
-# 	// dimensions of the data
-# 	int<lower = 0> N;
-# 	int<lower = 0> P;
+# read in the posterior draws for each parameter
+param_file = "/tmp/tmpo3u2spqi/output.csv"
+advi_coef = pd.read_csv(param_file 
+	,skiprows = [0, 1, 2, 3, 5, 6]
+	)
 
-# 	// predictors (x) and target (y)
-# 	matrix[N, P] x;
-# 	int<lower = 0, upper = 1> y[N];
-# }
+print(np.arange(1, p, int(p/true_p)) + 1)
 
-# parameters {
-# 	// regression coefficients
-# 	real alpha;
-# 	vector[P] beta;
+betas = advi_coef.filter(like = "beta")
+betas = betas.mean(axis = 0)
+print(abs(betas).sort_values()[-10:])
 
-# 	// horseshoe prior parameters
-# 	vector<lower = 0>[P] lambda;
-# 	real<lower = 0> tau;
+# print(sorted(betas.argsort()[-10:][::-1]))
 
-# 	// epsilon parameters
-# 	real<lower = 0> sigma; //std
-# }
-
-# model {
-# 	// construct horseshoe prior on the betas
-# 	lambda ~ cauchy(0, 1);
-# 	tau ~ cauchy(0, 1);
-# 	beta ~ normal(0, lambda * tau);
-
-# 	//for(p in 1:P)
-# 	//  beta[p] ~ normal(0, lambda[p] * tau);
-
-# 	// construct model 
-# 	y ~ bernoulli_logit(alpha + x * beta);
-# }
-# '''
-
-# gwas_data = {
-# 	'N': n
-# 	,'P': p
-# 	,'y': geno
-# 	,'x': snps
-# }
-
-# model = StanModel(model_code = gwas_code)
-
-# # ADVI using Stochastic gradient descent
-# fit_advi = model.vb(data = gwas_data
-# 	,output_samples = 2000
-# 	,iter = 20000
-# 	,algorithm = 'meanfield')
-
-
-# # read in the posterior draws for each parameter
-# param_file = fit_advi['args']['sample_file'].decode("utf-8")
-# advi_coef = pd.read_csv(param_file 
-# 	,skiprows = [0, 1, 2, 3, 5, 6]
-# 	)
-
-# betas = advi_coef.filter(like = "beta")
-# betas = betas[betas.apply(lambda x: )]
-
-# # note that the beta number is snps number + 1
-# # TODO: definitely need a more efficient plot for this many parameters...
-# sb.violinplot(data = advi_coef.filter(like = "beta"))
+# sb.tsplot(data = betas, interpolate = False)
+# # sb.distplot(betas['beta.51'])
 # sb.plt.show()
+# print(betas.mean(axis = 0))
+
+# top_betas = np.where(abs(betas) > 0.9)
+# print(top_betas)
--- model.py
+++ model.py
@@ -6,9 +6,9 @@ from pystan import StanModel
 from math import exp
 
 # data shape
-p = 1000
+p = 3000
 true_p = 10
-n = 100
+n = 1500
 
 # generate SNPs
 maf = np.random.uniform(0.05, 0.5, p)
@@ -33,7 +33,7 @@ def binomialize(x):
 	return np.random.binomial(1, p, 1)
 vbin = np.vectorize(binomialize)
 
-geno = np.sum(true_alpha + true_snps * true_beta, axis = 1)
+geno = true_alpha + np.sum(true_snps * true_beta, axis = 1)
 geno = vbin(geno)
 
 
@@ -54,22 +54,21 @@ parameters {
 	real alpha;
 	vector[P] beta;
 
-	// horseshoe prior parameters
-	vector<lower = 0>[P] lambda;
-	real<lower = 0> tau;
-
-	// epsilon parameters
-	real<lower = 0> sigma; //std
+	// horseshoe+ prior parameters
+	//vector<lower = 0>[P] lambda;
+	//vector<lower = 0>[P] eta;
+	//real<lower = 0> tau;
 }
 
 model {
-	// construct horseshoe prior on the betas
-	lambda ~ cauchy(0, 1);
-	tau ~ cauchy(0, 1);
-	beta ~ normal(0, lambda * tau);
+	// construct horseshoe+ prior on the betas...this has to be wrong..
+	//tau ~ cauchy(0, 1);
+	//eta ~ cauchy(0, 1);
+	//lambda ~ cauchy(0, 1);
+	//beta ~ normal(0, lambda .* eta * tau);
 
-	//for(p in 1:P)
-	//  beta[p] ~ normal(0, lambda[p] * tau);
+	// instead use the laplace prior to regularize. Much better convergence.
+	beta ~ double_exponential(0, 1);
 
 	// construct model 
 	y ~ bernoulli_logit(alpha + x * beta);
@@ -85,12 +84,11 @@ gwas_data = {
 
 model = StanModel(model_code = gwas_code)
 
-# ADVI using Stochastic gradient descent
+# ADVI using stochastic(?) gradient descent
 fit_advi = model.vb(data = gwas_data
 	,output_samples = 2000
-	,iter = 20000
+	,iter = 10000
 	,algorithm = 'meanfield')
-
 
 # read in the posterior draws for each parameter
 param_file = fit_advi['args']['sample_file'].decode("utf-8")
@@ -98,10 +96,18 @@ advi_coef = pd.read_csv(param_file
 	,skiprows = [0, 1, 2, 3, 5, 6]
 	)
 
+print(param_file)
+
 # betas = advi_coef.filter(like = "beta")
 # betas = betas[betas.apply(lambda x: )]
 
 # # note that the beta number is snps number + 1
-# # TODO: definitely need a more efficient plot for this many parameters...
-# sb.violinplot(data = advi_coef.filter(like = "beta"))
+print(true_beta)
+# sb.boxplot(data = advi_coef.filter(like = "beta"), showfliers=False)
 # sb.plt.show()
+
+betas = advi_coef.filter(like = "beta")
+betas = betas.mean(axis = 0)
+print(np.arange(1, p, int(p/true_p)) + 1)
+print(abs(betas).sort_values()[-20:])
+
